2b45b9fa5953e01910c0a15c49b2e29b529c8ba2
Ketan Kumar Seth <ketankrseth@gmail.com>
2026-06-23 22:25:52 +0530

    Add files via upload

---
+++ Exp7/src/App.jsx
@@ -0,0 +1,22 @@
+
+import Student from "./Student";
+function App() {
+return (
+<div>
+<h1>Student Information</h1>
+<Student
+name="Gramin Prasad"
+course="Computer Science"
+marks="55" />
+<Student
+name="Aniket Goswami"
+course="Information Technology"
+marks="90" />
+<Student
+name="Faisal Khan"
+course="Electronics"
+marks="78" />
+</div>
+);
+}
+export default App;
